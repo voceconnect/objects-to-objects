761e3e308228afac7236dea074626f49e7ce0e7a
= <mpretty@voceconnect.com>
2015-07-16 10:57:09 -0400

    adding handling to prevent fatal errors for installations that load dependencies before WP

---
+++ loader.php
@@ -0,0 +1,29 @@
+<?php
+
+if ( !class_exists( 'Composer\Autoload\ClassLoader' ) ) {
+	//loadd classes if library not managed by composer
+	if ( !class_exists( 'O2O' ) ) {
+		require_once ( __DIR__ . '/src/o2o.php' );
+	}
+
+	if ( !class_exists( 'O2O_Connection_Factory' ) ) {
+		require_once ( __DIR__ . '/src/factory.php' );
+	}
+
+	if ( !class_exists( 'O2O_Query' ) ) {
+		require_once ( __DIR__ . '/src/query.php' );
+	}
+
+	if ( !class_exists( 'O2O_Rewrites' ) ) {
+		require_once ( __DIR__ . '/src/rewrites.php' );
+	}
+
+	if ( !class_exists( 'O2O_Connection_Taxonomy' ) ) {
+		require_once ( __DIR__ . '/src/connection-types/taxonomy/taxonomy.php' );
+	}
+}
+
+$o2oInstance = O2O::GetInstance();
+if ( !has_action( 'init', [$o2oInstance, 'init' ] ) ) {
+	add_action( 'init', array( O2O::GetInstance(), 'init' ), 20 );
+}
--- objects-to-objects.php
+++ objects-to-objects.php
@@ -1,31 +1,14 @@
 <?php
+
 /*
   Plugin Name: Objects to Objects
-  Version:     1.3.1
+  Version:     1.4.0
   Plugin URI:  http://voceplatforms.com
   Description: A WordPress plugin/module that provides the ability to map relationships between posts and other post types.
   Author:      Voce Platforms
   Author URI:  http://voceplatforms.com/
  */
 
-if ( !class_exists( 'O2O' ) ) {
-	require_once ( __DIR__ . '/src/o2o.php' );
+if ( defined( 'ABSPATH' ) ) {
+  require_once __DIR__ . '/loader.php';
 }
-
-if ( !class_exists( 'O2O_Connection_Factory' ) ) {
-	require_once ( __DIR__ . '/src/factory.php' );
-}
-
-if ( !class_exists( 'O2O_Query' ) ) {
-	require_once ( __DIR__ . '/src/query.php' );
-}
-
-if ( !class_exists( 'O2O_Rewrites' ) ) {
-	require_once ( __DIR__ . '/src/rewrites.php' );
-}
-
-if ( !class_exists( 'O2O_Connection_Taxonomy' ) ) {
-	require_once ( __DIR__ . '/src/connection-types/taxonomy/taxonomy.php' );
-}
-
-add_action( 'init', array( O2O::GetInstance(), 'init' ), 20 );
