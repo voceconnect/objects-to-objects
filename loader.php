53e007d1e6651523faa3caaaa4269dd0821f881f
Philip John <phil@philipjohn.co.uk>
2016-04-06 13:20:45 +0100

    Explicitly load Composer's autoloader if it exists, fall back to loading classes manually

--- loader.php
+++ loader.php
@@ -1,29 +1,33 @@
 <?php
 
-if ( !class_exists( 'Composer\Autoload\ClassLoader' ) ) {
-	//loadd classes if library not managed by composer
-	if ( !class_exists( 'O2O' ) ) {
+// Use Composer's autoloader if it exists
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once( __DIR__ . '/vendor/autoload.php' );
+}
+// Otherwise, load classes manually
+else {
+	if ( ! class_exists( 'O2O' ) ) {
 		require_once ( __DIR__ . '/src/o2o.php' );
 	}
 
-	if ( !class_exists( 'O2O_Connection_Factory' ) ) {
+	if ( ! class_exists( 'O2O_Connection_Factory' ) ) {
 		require_once ( __DIR__ . '/src/factory.php' );
 	}
 
-	if ( !class_exists( 'O2O_Query' ) ) {
+	if ( ! class_exists( 'O2O_Query' ) ) {
 		require_once ( __DIR__ . '/src/query.php' );
 	}
 
-	if ( !class_exists( 'O2O_Rewrites' ) ) {
+	if ( ! class_exists( 'O2O_Rewrites' ) ) {
 		require_once ( __DIR__ . '/src/rewrites.php' );
 	}
 
-	if ( !class_exists( 'O2O_Connection_Taxonomy' ) ) {
+	if ( ! class_exists( 'O2O_Connection_Taxonomy' ) ) {
 		require_once ( __DIR__ . '/src/connection-types/taxonomy/taxonomy.php' );
 	}
 }
 
 $o2oInstance = O2O::GetInstance();
-if ( !has_action( 'init', array( $o2oInstance, 'init' ) ) ) {
+if ( ! has_action( 'init', array( $o2oInstance, 'init' ) ) ) {
 	add_action( 'init', array( O2O::GetInstance(), 'init' ), 20 );
 }
